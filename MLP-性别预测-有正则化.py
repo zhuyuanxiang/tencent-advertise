6d3d0639cc7e2d08b666218c7bc92a061b048707
zYx.Tom <zhuyuanxiang@gmail.com>
2020-05-28 18:59:40 +0800

    修改了 csv 的读取 修改了 One-Hot 编码 修改了输出的理解 修改错误的变量名称

--- MLP-性别预测-有正则化.py
+++ MLP-性别预测-有正则化.py
@@ -24,23 +24,46 @@ from sklearn.model_selection import train_test_split
 # 「CSV」文件字段名称
 # "creative_id","click_times","ad_id","product_id","product_category","advertiser_id","industry",
 filename = './data/all_log_300k.csv'
-csv_data = pd.read_csv(filename)
-csv_data = np.array(csv_data)
-print(csv_data.shape)
-csv_data = csv_data[:1500000, :]
-X = csv_data[:, 1:8]
-y = csv_data[:, 9].reshape([X.shape[0], 1])
-y_onehot = np.zeros([y.shape[0], 2])
-for i in range(y.shape[0]):
+df = pd.read_csv(filename)
+y_data = df['gender'].values - 1  # 性别作为目标数据
+X_data = df[[
+        "creative_id",
+        "click_times",
+        "ad_id",
+        "product_id",
+        "product_category",
+        "advertiser_id",
+        "industry",
+]].values
+X = X_data
+y = y_data
 
-    if y[i, 0] == 1:
-        y_onehot[i, 0] = 0
-    if y[i, 0] == 2:
-        y_onehot[i, 1] = 1
-    if i % 10000 == 0:
-        print(i)
+# 编码目标数据，使用 sklearn 的 One-Hot 编码
+from sklearn.preprocessing import OneHotEncoder
 
-y = y_onehot
+enc = OneHotEncoder()
+y_one_hot = enc.fit_transform(y.reshape(-1, 1)).toarray()
+# y_one_hot = enc.fit_transform(np.concatenate((y_train, y_test)).reshape(-1, 1)).toarray()
+# y_train_one_hot = y_one_hot[:train_len]
+# y_test_one_hot = y_one_hot[train_len:]
+
+# csv_data = pd.read_csv(filename)
+# csv_data = np.array(csv_data)
+# print(csv_data.shape)
+# csv_data = csv_data[:1500000, :]
+# X = csv_data[:, 1:8]
+# y = csv_data[:, 9].reshape([X.shape[0], 1])
+# y_one_hot = np.zeros([y.shape[0], 2])
+# for i in range(y.shape[0]):
+#     if y[i, 0] == 1:
+#         # TODO:这个One-Hot 编码是错的
+#         y_one_hot[i, 0] = 0
+#     if y[i, 0] == 2:
+#         y_one_hot[i, 1] = 1
+#     if i % 10000 == 0:
+#         print(i)
+
+y = y_one_hot
 X = preprocessing.scale(X)
 
 random_state = 0
@@ -49,7 +72,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, strat
 y_train = y_train.astype(np.float)
 y_test = y_test.astype(np.float)
 
-print(y_train.shape)
+print("y_train.shape =",y_train.shape)
 
 max_steps = 50000
 learing_rate = 0.00007
@@ -85,9 +108,9 @@ with tf.name_scope('train'):
     y_label = tf.placeholder(tf.float32, [None, 2], name = 'y_label')
     hidden1, h_L_1 = create_layer(x, 7, 256, 'layer1')
 
-    hinden2, h_L_2 = create_layer(hidden1, 256, 100, 'hinden2')
+    hidden2, h_L_2 = create_layer(hidden1, 256, 100, 'hidden2')
 
-    gender_age_pre, h_L_3 = create_layer(hinden2, 100, 2, 'gender_age')
+    gender_age_pre, h_L_3 = create_layer(hidden2, 100, 2, 'gender_age')
 
     loss = tf.nn.softmax_cross_entropy_with_logits(logits = gender_age_pre, labels = y_label)
     loss = tf.reduce_mean(loss) + h_L_1 + h_L_2 + h_L_3
@@ -100,11 +123,11 @@ acc = tf.reduce_mean(tf.cast(pre, tf.float32))
 
 merged = tf.summary.merge_all()
 saver = tf.train.Saver()
-print('a')
+print('Saver 定义完成')
 
 with tf.Session() as sess:
     with tf.device("/cpu:0"):
-        print(i)
+        # print(i)  # TODO:为什么要打的 i？
         init = tf.global_variables_initializer()
 
         sess.run(init)  # 初始化全局
