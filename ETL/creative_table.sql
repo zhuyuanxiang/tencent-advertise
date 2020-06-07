9f125035674eef1a993b372ec99367083aca36cc
zYx.Tom <zhuyuanxiang@gmail.com>
2020-06-07 11:12:42 +0800

    以前提取部分数据的方法是错误的 基于 product_id 提取数据会导致数据的概率分布的改变 现在使用每个用户访问素材的数量来控制数据的分布，可以跟模型建立起关系
    Signed-off-by: zYx.Tom <zhuyuanxiang@gmail.com>

--- ETL/creative_table.sql
+++ ETL/creative_table.sql
@@ -28,179 +28,106 @@ CREATE TABLE `user_list` (
 ) ENGINE = MyISAM COMMENT = 'user.csv' DELAY_KEY_WRITE = 1;
 
 /* C2. 创建辅助数据表 */
-/* 创建非空值的素材表 */
-CREATE TABLE `ad_valid` (
-    `creative_id` int NOT NULL,
-    `ad_id` int NOT NULL,
-    `product_id` int NOT NULL,
-    `product_category` int NOT NULL,
-    `advertiser_id` int NOT NULL,
-    `industry` int NOT NULL
-) ENGINE = MyISAM COMMENT = 'product_id>0 and industry>0' DELAY_KEY_WRITE = 1;
+/* 创建基于内存的表`number_id`，用于保存统计的 `xxx_id`的数据 */
+DROP TABLE number_id_all;
 
-INSERT INTO
-    ad_valid
-SELECT
-    *
-FROM
-    ad_list
-WHERE
-    product_id > 0
-    AND industry > 0;
-
-ALTER TABLE
-    `ad_valid`
-ADD
-    INDEX `creative_id_idx` (`creative_id`) USING BTREE;
-
-/* 创建非空值的日志表(包含全部字段) 数据量：16411005*/
-CREATE TABLE `all_log_valid`(
-    `time_id` int NOT NULL,
-    `user_id` int NOT NULL,
-    `creative_id` int NOT NULL,
-    `click_times` int NOT NULL,
-    `ad_id` int NOT NULL,
-    `product_id` int NOT NULL,
-    `product_category` int NOT NULL,
-    `advertiser_id` int NOT NULL,
-    `industry` int NOT NULL,
-    `age` int NOT NULL,
-    `gender` int NOT NULL
-) ENGINE = MyISAM COMMENT = '所有的字段合并在一起' DELAY_KEY_WRITE = 1;
-
-INSERT INTO
-    `all_log_valid`
-SELECT
-    A.time_id,
-    A.user_id,
-    A.creative_id,
-    A.click_times,
-    B.ad_id,
-    B.product_id,
-    B.product_category,
-    B.advertiser_id,
-    B.industry,
-    C.age,
-    C.gender
-FROM
-    click_log AS A
-    INNER JOIN ad_valid AS B ON B.creative_id = A.creative_id
-    INNER JOIN user_list AS C ON C.user_id = A.user_id;
-
-ALTER TABLE
-    `all_log_valid`
-ADD
-    INDEX `creative_id_idx` (`creative_id`) USING BTREE,
-ADD
-    INDEX `user_id_idx` (`user_id`) USING BTREE,
-ADD
-    INDEX `product_id_idx` (`product_id`) USING BTREE;
-
-/* 创建基于内存表的`product_id`的有效数据表，用于保存临时统计的 `product_id`数据 */
-CREATE TABLE `product_id_count_number` (
-    `product_id` int NOT NULL,
-    `product_id_number` int NULL,
-    PRIMARY KEY (`product_id`),
-    INDEX `product_id_number_idx` (`product_id_number`) USING BTREE
-) ENGINE = MEMORY;
+CREATE TABLE number_id_all (
+    `id_inc` int NOT NULL AUTO_INCREMENT,
+    `id` int NULL,
+    `id_number` int NULL,
+    PRIMARY KEY (`id_inc`)
+) ENGINE = MyISAM STORAGE MEMORY;
 
 /* 注：因为是内存表，每次系统重启后，如果还要使用这张表，需要重新插入数据，大约60秒完成 */
 INSERT INTO
-    product_id_count_number
+    number_id_all(id, id_number)
 SELECT
-    product_id,
-    COUNT(1) AS product_id_number
+    user_id AS id,
+    COUNT(1) AS id_number
 FROM
-    all_log_valid
+    click_log
 GROUP BY
-    product_id;
+    user_id
+ORDER BY
+    id_number;
 
-/* 创建 700万 的有效数据表 数据量：7152231*/
-CREATE TABLE `all_log_valid_7m`(
-    `time_id` int NOT NULL,
-    `user_id` int NOT NULL,
-    `creative_id` int NOT NULL,
-    `click_times` int NOT NULL,
-    `ad_id` int NOT NULL,
-    `product_id` int NOT NULL,
-    `product_category` int NOT NULL,
-    `advertiser_id` int NOT NULL,
-    `industry` int NOT NULL,
-    `age` int NOT NULL,
-    `gender` int NOT NULL
-) ENGINE = MyISAM COMMENT = '所有的字段合并在一起，700万条数据' DELAY_KEY_WRITE = 1;
+ALTER TABLE
+    number_id_all
+ADD
+    INDEX `id_idx`(`id`) USING BTREE ，
+ADD
+    INDEX `id_number_idx`(`id_number`) USING BTREE;
 
-INSERT INTO
-    `all_log_valid_7m`
+/* 
+ 创建 50 万的数据表，标准是每个用户访问素材数目小于11
+ 用户数：
+ 素材数：
+ 数据条数： 
+ 注：还可以小于 21 创建 500万数据表；小于 51 创建 1500 万数据表
+ */
+CREATE TABLE click_log_500k
 SELECT
     A.*
 FROM
-    `all_log_valid` as A
-    INNER JOIN `product_id_count_number` AS B ON B.`product_id` = A.`product_id`
+    click_log AS A,
+    number_id_all AS B
 WHERE
-    B.`product_id_number` BETWEEN 200
-    AND 70000;
+    B.id_number < 11
+    AND A.user_id = B.id;
 
 ALTER TABLE
-    `all_log_valid_7m`
+    click_log_500k
 ADD
-    INDEX `creative_id_idx` (`creative_id`) USING BTREE,
-ADD
-    INDEX `user_id_idx` (`user_id`) USING BTREE,
-ADD
-    INDEX `product_id_idx` (`product_id`) USING BTREE;
+    PRIMARY KEY (`time_id`, `user_id`, `creative_id`);
 
-/* 创 建 大 约 300 万 有 效 数 据  */
-CREATE TABLE `all_log_valid_3m`(
-    `time_id` int NOT NULL,
-    `user_id` int NOT NULL,
-    `creative_id` int NOT NULL,
-    `click_times` int NOT NULL,
-    `ad_id` int NOT NULL,
-    `product_id` int NOT NULL,
-    `product_category` int NOT NULL,
-    `advertiser_id` int NOT NULL,
-    `industry` int NOT NULL,
-    `age` int NOT NULL,
-    `gender` int NOT NULL
-) ENGINE = MyISAM COMMENT = '所有的字段合并在一起，300万条数据' DELAY_KEY_WRITE = 1;
-
-INSERT INTO
-    `all_log_valid_3m`
+/* 
+ 创建 100 万的数据表，标准是每个用户访问素材数目以 450 为均值
+ 用户数：
+ 素材数：
+ 数据条数： 
+ */
+CREATE TABLE click_log_1m
 SELECT
     A.*
 FROM
-    `all_log_valid_7m` as A
-    INNER JOIN `product_id_count_number` AS B ON B.`product_id` = A.`product_id`
+    click_log AS A,
+    number_id_all AS B
 WHERE
-    B.`product_id_number` BETWEEN 400
-    AND 10000;
+    B.id_number BETWEEN 200
+    AND 700
+    AND A.user_id = B.id;
 
 ALTER TABLE
-    `all_log_valid_3m`
+    click_log_1m
 ADD
-    INDEX `creative_id_idx` (`creative_id`) USING BTREE,
-ADD
-    INDEX `user_id_idx` (`user_id`) USING BTREE,
-ADD
-    INDEX `product_id_idx` (`product_id`) USING BTREE;
+    PRIMARY KEY (`time_id`, `user_id`, `creative_id`);
 
-/*创 建 大 约 100 万 有 效 数 据*/
-CREATE TABLE all_log_valid_1m AS
+/* 
+ 创建 500 万的数据表
+ 用户数：
+ 素材数：
+ 数据条数： 
+ */
+CREATE TABLE click_log_5m
 SELECT
     A.*
 FROM
-    `all_log_valid_3m` as A
-    INNER JOIN `product_id_count_number` AS B ON B.`product_id` = A.`product_id`
+    click_log AS A,
+    number_id_all AS B
 WHERE
-    B.`product_id_number` BETWEEN 400
-    AND 2100;
+    A.user_id = B.id
+    AND 
+    B.id_number BETWEEN 100
+    AND 800   ;
 
 ALTER TABLE
-    `all_log_valid_1m`
+    click_log_5m
 ADD
-    INDEX `creative_id_idx` (`creative_id`) USING BTREE,
-ADD
-    INDEX `user_id_idx` (`user_id`) USING BTREE,
-ADD
-    INDEX `product_id_idx` (`product_id`) USING BTREE;
+    PRIMARY KEY (`time_id`, `user_id`, `creative_id`);
+
+/* 
+ 创建 1500 万的数据表
+ 用户数：
+ 素材数：
+ 数据条数： 
+ */
