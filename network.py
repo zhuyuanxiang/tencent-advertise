0376e8e5524e49c1841d5064c5ae8e46bd8dfb1c
zYx.Tom <zhuyuanxiang@gmail.com>
2020-06-11 18:12:12 +0800

    重构「网络模型构建」函数，将各种函数统一为参数配置

--- network.py
+++ network.py
@@ -22,11 +22,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import winsound
-from tensorflow import keras
-from tensorflow.python.keras.activations import relu, sigmoid, softmax
+from tensorflow.python.keras.layers import GlobalMaxPooling1D
 from tensorflow.python.keras.models import Sequential
-from tensorflow.python.keras.layers import Embedding
+from tensorflow.python.keras.layers import Bidirectional, Conv1D, Dropout, Embedding, Flatten, LSTM
 from tensorflow.python.keras.layers import Dense
+from tensorflow.python.keras.regularizers import l2
+from tensorflow.python.keras import optimizers
+from tensorflow.python.keras import losses
+from tensorflow.python.keras import metrics
 
 # ----------------------------------------------------------------------
 plt.rcParams['font.sans-serif'] = ['YaHei Consolas Hybrid']  # 用来正常显示中文标签
@@ -47,94 +50,72 @@ assert sklearn.__version__ >= "0.20"
 assert np.__version__ >= "1.18.1"
 
 
-# ----------------------------------------------------------------------
-def construct_mlp(creative_id_num, embedding_size, max_len):
-    from tensorflow.python.keras.layers import Flatten
+def construct_model(creative_id_num, embedding_size, max_len, RMSProp_lr,
+                    model_type = "MLP", label_name = "gender"):
+    '''
+        构建与编译模型
+    :param creative_id_num: 字典容量
+    :param embedding_size: 嵌入维度
+    :param max_len: 序列长度
+    :param RMSProp_lr: 学习步长
+    :param model_type: 模型的类型
+        MLP：多层感知机
+        Conv1D：1维卷积神经网络
+        GlobalMaxPooling1D：1维全局池化层
+        GlobalMaxPooling1D+MLP：1维全局池化层+多层感知机
+        Conv1D+LSTM：1维卷积神经网络+LSTM
+        Bidirectional+LSTM：双向 LSTM
+    :param label_name: 标签的类型
+        age ： 根据年龄进行的多分类问题
+        gender : 根据性别进行的二分类问题
+    :return: 返回构建的模型
+    '''
     print("* 构建网络")
     model = Sequential()
     model.add(Embedding(creative_id_num, embedding_size, input_length = max_len))
-    model.add(Flatten())
-    model.add(keras.layers.Dense(
-            8, activation = keras.activations.relu,
-            kernel_regularizer = keras.regularizers.l2(0.001)
-    ))
-    model.add(keras.layers.Dropout(0.5))
-    model.add(keras.layers.Dense(
-            4, activation = keras.activations.relu,
-            kernel_regularizer = keras.regularizers.l2(0.001)
-    ))
-    model.add(keras.layers.Dropout(0.5))
-    model.add(Dense(1, activation = 'sigmoid'))
-    print("MLP——模型构建完成！")
-    return model
+    if model_type == 'MLP':
+        model.add(Flatten())
+        model.add(Dense(8, activation = 'relu', kernel_regularizer = l2(0.001)))
+        model.add(Dropout(0.5))
+        model.add(Dense(4, activation = 'relu', kernel_regularizer = l2(0.001)))
+        model.add(Dropout(0.5))
+    elif model_type == 'Conv1d':
+        model.add(Conv1D(32, 7, activation = 'relu', kernel_regularizer = l2(0.001)))
+        model.add(Conv1D(32, 7, activation = 'relu', kernel_regularizer = l2(0.001)))
+        model.add(GlobalMaxPooling1D())
+    elif model_type == 'GlobalMaxPooling1D':
+        model.add(GlobalMaxPooling1D())
+    elif model_type == 'GlobalMaxPooling1D+MLP':
+        model.add(GlobalMaxPooling1D())
+        model.add(Dense(64, activation = 'relu', kernel_regularizer = l2(0.001)))
+        # model.add(Dropout(0.5))
+        model.add(Dense(32, activation = 'relu', kernel_regularizer = l2(0.001)))
+        # model.add(Dropout(0.5))
+    elif model_type == 'Conv1d+LSTM':
+        model.add(Conv1D(32, 5, activation = 'relu', kernel_regularizer = l2(0.001)))
+        model.add(Conv1D(32, 5, activation = 'relu', kernel_regularizer = l2(0.001)))
+        model.add(LSTM(16, dropout = 0.5, recurrent_dropout = 0.5))
+    elif model_type == 'Bidirectional-LSTM':
+        model.add(Bidirectional(LSTM(embedding_size, dropout = 0.2, recurrent_dropout = 0.2)))
 
+    if label_name == "age":
+        model.add(Dense(10, activation = 'softmax'))
+        print("%s——模型构建完成！" % model_type)
+        print("* 编译模型")
+        model.compile(optimizer = optimizers.RMSprop(lr = RMSProp_lr),
+                      loss = losses.sparse_categorical_crossentropy,
+                      metrics = [metrics.sparse_categorical_accuracy])
+    elif label_name == 'gender':
+        model.add(Dense(1, activation = 'sigmoid'))
+        print("%s——模型构建完成！" % model_type)
+        print("* 编译模型")
+        model.compile(optimizer = optimizers.RMSprop(lr = RMSProp_lr),
+                      loss = losses.binary_crossentropy,
+                      metrics = [metrics.binary_accuracy])
+    else:
+        raise Exception("错误的标签类型！")
 
-# ----------------------------------------------------------------------
-def construct_Conv1d(creative_id_num, embedding_size, max_len):
-    from tensorflow.python.keras.layers import Conv1D
-    from tensorflow.python.keras.layers import GlobalMaxPooling1D, MaxPooling1D
-    print("* 构建网络")
-    model = Sequential()
-    model.add(Embedding(creative_id_num, embedding_size, input_length = max_len))
-    model.add(Conv1D(32, 7, activation = relu, kernel_regularizer = keras.regularizers.l2(0.001)))
-    model.add(Conv1D(32, 7, activation = relu, kernel_regularizer = keras.regularizers.l2(0.001)))
-    model.add(GlobalMaxPooling1D())
-    model.add(Dense(1, activation = sigmoid))
-    print("Conv1D——模型构建完成！")
-    return model
-
-
-# ----------------------------------------------------------------------
-def construct_GlobalMaxPooling1D(creative_id_num, embedding_size, max_len):
-    from tensorflow.python.keras.layers import Conv1D
-    from tensorflow.python.keras.layers import GlobalMaxPooling1D, MaxPooling1D
-    print("* 构建网络")
-    model = Sequential()
-    model.add(Embedding(creative_id_num, embedding_size, input_length = max_len))
-    model.add(GlobalMaxPooling1D())
-    model.add(Dense(1, activation = sigmoid))
-    model.add(Dense(10, activation = softmax))
-    print("Conv1D——模型构建完成！")
-    return model
-
-
-# ----------------------------------------------------------------------
-def construct_Conv1d_LSTM(creative_id_num, embedding_size, max_len):
-    from tensorflow.python.keras.layers import GlobalMaxPooling1D, MaxPooling1D
-    from tensorflow.python.keras.layers import Conv1D
-    from tensorflow.python.keras.layers import GRU, LSTM
-    print("* 构建网络")
-    model = Sequential()
-    model.add(Embedding(creative_id_num, embedding_size, input_length = max_len))
-    model.add(Conv1D(32, 5, activation = relu, kernel_regularizer = keras.regularizers.l2(0.001)))
-    model.add(Conv1D(32, 5, activation = relu, kernel_regularizer = keras.regularizers.l2(0.001)))
-    model.add(LSTM(16, dropout = 0.5, recurrent_dropout = 0.5))
-    model.add(Dense(1, activation = sigmoid))
-    print("Conv1D + LSTM——模型构建完成！")
-    return model
-
-
-# ----------------------------------------------------------------------
-def construct_LSTM(creative_id_num, embedding_size, max_len):
-    from tensorflow.python.keras.layers import LSTM
-    print("* 构建网络")
-    model = Sequential()
-    model.add(Embedding(creative_id_num, embedding_size, input_length = max_len))
-    model.add(LSTM(32, dropout = 0.5, recurrent_dropout = 0.5))
-    model.add(Dense(1, activation = sigmoid))
-    print("LSTM——模型构建完成！")
-    return model
-
-
-# ----------------------------------------------------------------------
-def construct_Bidirectional_LSTM(creative_id_num, embedding_size, max_len):
-    from tensorflow.python.keras.layers import Bidirectional, LSTM
-    print("* 构建网络")
-    model = Sequential()
-    model.add(Embedding(creative_id_num, embedding_size, input_length = max_len))
-    model.add(Bidirectional(LSTM(embedding_size, dropout = 0.2, recurrent_dropout = 0.2)))
-    model.add(Dense(1, activation = sigmoid))
-    print("Bidirectional LSTM——模型构建完成！")
+    print(model.summary())
     return model
 
 
