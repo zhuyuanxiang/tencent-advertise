47b7bef618a25dd93dedab9770edec6cd9fc7e98
zYx.Tom <zhuyuanxiang@gmail.com>
2020-07-03 10:08:47 +0800

    训练 Word2Vec 模块

--- Word2Vec.py
+++ Word2Vec.py
@@ -11,9 +11,10 @@
 @Time       :   2020-06-26 18:54
 @License    :   (C)Copyright 2018-2020, zYx.Tom
 @Reference  :   
-@Desc       :   
+@Desc       :   训练 Word2Vec 模块
 @理解：
 """
+import pickle
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -25,99 +26,51 @@ from gensim.models import Word2Vec
 seed = 42
 
 
-# =====================================================
-# 专门用于 Word2Vec 训练使用的数据集
-def data_word2vec() -> list:
-    # 载入数据需要的变量
-    file_name = './data/train_data_all_min_complete_v.csv'
-    field_list = [  # 输入数据处理：选择需要的列
-        "user_id_inc",  # 0
-        "creative_id_inc",  # 1
-        "time_id",  # 2
-    ]
+# ----------------------------------------------------------------------
+def train_word2vec_model_with_gensim(creative_id_window, file_name):
+    print('-' * 5 + "   载入数据集: {0}   ".format(file_name) + '-' * 5)
+    fname = open(file_name, 'rb')
+    words_lists: list = pickle.load(fname)
+    for one_list in words_lists:
+        for j, number in enumerate(one_list):
+            one_list[j] = number
+    print('-' * 5 + "   训练 word2vec 模型   " + '-' * 5)
+    model = Word2Vec(
+        words_lists,
+        size=embedding_size,
+        window=embedding_window,
+        min_count=1,
+        seed=seed,
+        workers=8,
+        sg=0,  # 0:CBOW;1:Skip-Gram
+        iter=10,
+        sorted_vocab=False,
+        batch_words=4096
+    )
+    print('-' * 5 + "   保存 word2vec 模型   " + '-' * 5)
+    model.save('save_model/word2vec/word2vec_{0}_{1}_{2}.model'.format(embedding_size, embedding_window, creative_id_window))
+    model.wv.save("save_model/word2vec/vectors_{0}_{1}_{2}.kv".format(embedding_size, embedding_window, creative_id_window))
 
-    print('-' * 5 + "   加载数据集   " + '-' * 5)
-    # 「CSV」文件字段名称
-    df = pd.read_csv(file_name, dtype=int)
-    # --------------------------------------------------
-    # 没有在数据库中处理索引，是因为尽量不在数据库中修正原始数据，除非是不得不变更的数据，这样子业务逻辑清楚
-    # user_id_inc:      X_csv[:,0]
-    # creative_id_inc:  X_csv[:,1]
-    # 索引在数据库中是从 1 开始的，在 Python 中是从 0 开始的，因此字段的偏移量为 -1
-    x_csv = df[field_list].values - 1
-    # 'creative_id_inc' 字段的偏移量为 3，是因为需要保留 {0, 1, 2}：0 表示 “padding”（填充），1 表示 “unknown”（未知词），2 表示 “start”（序列开始）
-    x_csv[:, 1] = x_csv[:, 1] + 3
-    print("数据加载完成。")
-
-    # --------------------------------------------------
-    print('-' * 5 + "   清洗数据集   " + '-' * 5)
-    data_length = x_csv.shape[0]
-    data_step = data_length // 100  # 标识数据清洗进度的步长
-    print("数据生成中（共 {0} 条数据)：".format(data_length), end='')
-    x_doc = np.zeros([user_id_num], dtype=list)
-
-    prev_user_id = -1  # -1 不在数据序列中
-    prev_time_id = -1  # 0 表示第 1 天
-    for i, row_data in enumerate(x_csv):
-        if (i % data_step) == 0:  # 数据清洗的进度
-            print("第 {0} 条数据-->".format(i), end=';')
-            pass
-        user_id = row_data[0]
-        time_id = row_data[2]
-
-        # 原始数据的 user_id 已经排序了，因此出现新的 user_id 时，就新建一个用户序列
-        if user_id > prev_user_id:
-            prev_user_id = user_id
-            prev_time_id = 0
-            x_doc[user_id]: list = [2]
-            pass
-
-        if (time_id - 1) > prev_time_id:  # 时间间隔超过 1 天的就插入 0
-            x_doc[user_id].append(0)
-
-        # row_data[0]: user_id
-        creative_id = row_data[1]
-        # row_data[2]: time_id
-
-        # 素材(creative_id)是关键
-        if creative_id_end > creative_id > creative_id_begin:
-            creative_id = creative_id - creative_id_begin
-        elif creative_id < creative_id_end - creative_id_max:
-            creative_id = creative_id_max - creative_id_begin + creative_id
-        else:
-            creative_id = 1  # 超过词典大小的素材标注为 1，即「未知」
-            pass
-
-        x_doc[user_id].append(creative_id)
-        prev_time_id = time_id
-    print("\n数据清洗完成！")
-    return x_doc.tolist()
-
-
-words_lists: list = data_word2vec()
-print(words_lists[10])
-for i, one_list in enumerate(words_lists):
-    for j, number in enumerate(one_list):
-        words_lists[i][j] = str(number)
-
-# 清洗数据需要的变量
-user_id_num = 900000  # 用户数
-creative_id_max = 2481135 - 1  # 最大的素材编号 = 素材的总数量 - 1，这个编号已经修正了数据库与Python索引的区别
-click_times_max = 152  # 所有素材中最大的点击次数
-time_id_max = 91
-creative_id_step_size = 128000
-creative_id_window = creative_id_step_size * 5
-creative_id_begin = creative_id_step_size * 0
-creative_id_end = creative_id_begin + creative_id_window
-embedding_size = 64
-embedding_window = 7
-model = Word2Vec(words_lists, sg=1, size=embedding_size, window=embedding_window, min_count=1, seed=seed, workers=8, iter=10, sorted_vocab=False,
-                 batch_words=4096)
-model.save('save_model/word2vec/word2vec_{0}_{1}_{2}.model'.format(embedding_size, embedding_window, creative_id_window))
-model.wv.save("save_model/word2vec/vectors_{0}_{1}_{2}.kv".format(embedding_size, embedding_window, creative_id_window))
 
 # =====================================================
 if __name__ == '__main__':
+    # 清洗数据需要的变量
+    user_id_num = 900000  # 用户数
+    creative_id_max = 2481135 - 1  # 最大的素材编号 = 素材的总数量 - 1，这个编号已经修正了数据库与Python索引的区别
+    click_times_max = 152  # 所有素材中最大的点击次数
+    time_id_max = 91
+    creative_id_step_size = 128000
+    embedding_size = 32
+    embedding_window = 5
+
+    creative_id_window = creative_id_step_size * 5
+    file_name = 'save_data/word2vec/no_interval/creative_id_{0}'.format(creative_id_window)
+    train_word2vec_model_with_gensim(creative_id_window, file_name)
+
+    creative_id_window = creative_id_step_size * 8
+    file_name = 'save_data/word2vec/with_interval/creative_id_{0}'.format(creative_id_window)
+    train_word2vec_model_with_gensim(creative_id_window, file_name)
+
     # 运行结束的提醒
     winsound.Beep(600, 500)
     plt.show()
