23e1e203222504547404a89c5b0aeeeebd9d03aa
zYx.Tom <zhuyuanxiang@gmail.com>
2020-07-05 10:13:35 +0800

    提取函数，训练无间隔与有间隔的数据，区别不大

--- Word2Vec.py
+++ Word2Vec.py
@@ -27,14 +27,8 @@ seed = 42
 
 
 # ----------------------------------------------------------------------
-def train_word2vec_model_with_gensim(creative_id_window, file_name):
-    print('-' * 5 + "   载入数据集: {0}   ".format(file_name) + '-' * 5)
-    fname = open(file_name, 'rb')
-    words_lists: list = pickle.load(fname)
-    for one_list in words_lists:
-        for j, number in enumerate(one_list):
-            one_list[j] = number
-    print('-' * 5 + "   训练 word2vec 模型   " + '-' * 5)
+def train_word2vec_model_with_gensim(words_lists):
+    print('-' * 5 + "   训练 word2vec({0}_{1}) 模型   ".format(embedding_size, embedding_window) + '-' * 5)
     model = Word2Vec(
         words_lists,
         size=embedding_size,
@@ -47,9 +41,31 @@ def train_word2vec_model_with_gensim(creative_id_window, file_name):
         sorted_vocab=False,
         batch_words=4096
     )
+    return model
+
+
+def load_words_list(words_path):
+    file_name = words_path + file_prefix + '_{0}'.format(creative_id_window)
+    print('-' * 5 + "   载入数据集: {0}   ".format(file_name) + '-' * 5)
+    fname = open(file_name, 'rb')
+    words_lists: list = pickle.load(fname)
+    for one_list in words_lists:
+        for j, number in enumerate(one_list):
+            one_list[j] = number
+    return words_lists
+
+
+def save_word2vec_model(model, file_name):
     print('-' * 5 + "   保存 word2vec 模型   " + '-' * 5)
-    model.save('save_model/word2vec/word2vec_{0}_{1}_{2}.model'.format(embedding_size, embedding_window, creative_id_window))
-    model.wv.save("save_model/word2vec/vectors_{0}_{1}_{2}.kv".format(embedding_size, embedding_window, creative_id_window))
+    model.save(file_name + '.model')
+    model.wv.save(file_name + '.kv')
+
+
+def main(path, window):
+    word2vec_words_lists = load_words_list('save_data/' + path)
+    word2vec_model = train_word2vec_model_with_gensim(word2vec_words_lists)
+    save_word2vec_model(word2vec_model,
+                        'save_model/' + path + file_prefix + "_{0}_{1}_{2}".format(embedding_size, embedding_window, creative_id_window))
 
 
 # =====================================================
@@ -60,16 +76,17 @@ if __name__ == '__main__':
     click_times_max = 152  # 所有素材中最大的点击次数
     time_id_max = 91
     creative_id_step_size = 128000
+    creative_id_window = creative_id_step_size * 5
     embedding_size = 32
     embedding_window = 5
+    file_prefix = 'creative_id'
+    no_interval_path = 'word2vec/no_interval/'
+    main(no_interval_path, creative_id_step_size * 5)
+    main(no_interval_path, creative_id_step_size * 8)
 
-    creative_id_window = creative_id_step_size * 5
-    file_name = 'save_data/word2vec/no_interval/creative_id_{0}'.format(creative_id_window)
-    train_word2vec_model_with_gensim(creative_id_window, file_name)
-
-    creative_id_window = creative_id_step_size * 8
-    file_name = 'save_data/word2vec/with_interval/creative_id_{0}'.format(creative_id_window)
-    train_word2vec_model_with_gensim(creative_id_window, file_name)
+    with_interval_path = 'word2vec/with_interval/'
+    main(with_interval_path, creative_id_step_size * 5)
+    main(with_interval_path, creative_id_step_size * 8)
 
     # 运行结束的提醒
     winsound.Beep(600, 500)
