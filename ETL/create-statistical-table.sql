93cc789f6a661dcaf4819efa5ceafe0af8fdb9e7
zYx.Tom <zhuyuanxiang@gmail.com>
2020-07-08 17:02:36 +0800

    整理文件内容，方便理解

--- ETL/create-statistical-table.sql
+++ ETL/create-statistical-table.sql
@@ -1,8 +1,9 @@
 /* C2. 生成数据的统计信息 */
+/* C2.1. 生成 tf_value, idf_value, tf_idf_value */
 /* 
  统计 click_log_all 中的 creative_id 素材
  sum_creative_id_times : 每个素材出现的次数 = tf_value
- sum_user_id_times：每个素材的访问用户数 = idf_value
+ sum_user_id_times ： 每个素材的访问用户数 = idf_value
  */
 UPDATE
     ad_list AS A,
@@ -22,6 +23,35 @@ SET
 WHERE
     A.creative_id = B.creative_id;
 
+/* 
+ 分别计算 `click_log_all` 中每个 `creative_id` 的 tf, idf, tf-idf 的值
+ tf : 素材出现的次数越多越重要，单个素材出现次数/所有素材出现次数
+ idf: 被访问的素材的用户数越少越重要，所有用户的数目/访问单个素材的用户数目
+ sparsity: 用户访问的素材越单一，这个素材的稀疏度就越高
+ */
+/* 
+ 更新 tf 值 : (A.sum_creative_id_times+1 / 30082771) AS tf_value  不使用除法，直接使用 最大值 排名
+ 更新 idf 值 idf_value = (LOG(900000 / A.sum_user_id_times+1))  不计算取对数，所有计算留到 tf-idf 中完成，直接使用 最小值 排名
+ 注： 增加 1 是为了避免出现 0
+ */
+UPDATE
+    ad_list
+SET
+    tf_value = sum_creative_id_times,
+    idf_value = sum_user_id_times;
+
+/* 
+ 计算 tf-idf 的值
+ tf_value + 1 : 防止 LOG() 计算得到 0
+ idf_value + 1 : 防止出现 0 作被除数，这个数据集中不存在这个问题
+ 900000 : 用户数（即文章的数目）
+ */
+UPDATE
+    ad_list
+SET
+    tf_idf_value = LOG(tf_value + 1) * (LOG(900000 / idf_value));
+
+/* C2.2. 生成 sparsity, sparsity_value */
 /* 
  统计 click_log_all 中的 user_id 的统计数据
  sum_creative_id_times : 每个用户访问素材的次数
@@ -62,34 +92,6 @@ SET
     A.sparsity = B.sum_creative_id_category
 WHERE
     A.user_id = B.user_id;
-
-/* 
- 分别计算 `click_log_all` 中每个 `creative_id` 的 tf, idf, tf-idf 的值
- tf : 素材出现的次数越多越重要，单个素材出现次数/所有素材出现次数
- idf: 被访问的素材的用户数越少越重要，所有用户的数目/访问单个素材的用户数目
- sparsity: 用户访问的素材越单一，这个素材的稀疏度就越高
- */
-/* 
- 更新 tf 值 : (A.sum_creative_id_times+1 / 30082771) AS tf_value  不使用除法，直接使用 最大值 排名
- 更新 idf 值 idf_value = (LOG(900000 / A.sum_user_id_times+1))  不计算取对数，所有计算留到 tf-idf 中完成，直接使用 最小值 排名
- 注： 增加 1 是为了避免出现 0
- */
-UPDATE
-    ad_list
-SET
-    tf_value = sum_creative_id_times,
-    idf_value = sum_user_id_times;
-
-/* 
- 计算 tf-idf 的值
- tf_value + 1 : 防止 LOG() 计算得到 0
- idf_value + 1 : 防止出现 0 作被除数，这个数据集中不存在这个问题
- 900000 : 用户数（即文章的数目）
- */
-UPDATE
-    ad_list
-SET
-    tf_idf_value = LOG(tf_value + 1) * (LOG(900000 / idf_value));
 
 /* 
  更新 sparsity 值 
