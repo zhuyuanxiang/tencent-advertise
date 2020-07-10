d7d8b4ec2364e12dc06bd6f05711c53c63e96ac9
zYx.Tom <zhuyuanxiang@gmail.com>
2020-07-10 11:12:17 +0800

    重构 load_word2vec_file() 函数

--- load_data.py
+++ load_data.py
@@ -14,8 +14,10 @@
 @Desc       :   数据载入模块
 @理解：
 """
+import os
+
 import pandas as pd
-from show_data import show_example_data, show_word2vec_data
+from show_data import show_example_data, show_word2vec_data, show_original_x_data
 
 seed = 42
 
@@ -53,14 +55,8 @@ def load_original_data(file_name, field_list, label_list):
 
 
 # --------------------------------------------------
-def load_word2vec_file():
+def load_word2vec_file(file_name, field_list):
     # 载入数据需要的变量
-    file_name = './data/train_data_all_min_complete_v.csv'
-    field_list = [  # 输入数据处理：选择需要的列
-        "user_id_inc",  # 0
-        "creative_id_inc",  # 1
-        "time_id",  # 2
-    ]
     print('-' * 5 + "   加载数据集:{0}   ".format(file_name) + '-' * 5)
     # 「CSV」文件字段名称
     df = pd.read_csv(file_name, dtype=int)
@@ -73,5 +69,5 @@ def load_word2vec_file():
     # 'creative_id_inc' 字段的偏移量为 3，是因为需要保留 {0, 1, 2}：0 表示 “padding”（填充），1 表示 “unknown”（未知词），2 表示 “start”（序列开始）
     x_csv[:, 1] = x_csv[:, 1] + 3
     print("数据加载完成。")
-    show_word2vec_data(x_csv)
+    show_original_x_data(x_csv)
     return x_csv
