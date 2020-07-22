571d85b766bd8e19bc9322814f3d61f896b7829b
zYx.Tom <zhuyuanxiang@gmail.com>
2020-07-22 18:42:34 +0800

    增加几个控制参数

--- config.py
+++ config.py
@@ -43,18 +43,21 @@ creative_id_end = creative_id_begin + creative_id_window
 # 参数说明：
 # model_type = "Bidirectional+LSTM"  # Bidirectional+LSTM：双向 LSTM
 # model_type = "Conv1D"  # Conv1D：1 维卷积神经网络
+# model_type = "LeNet"  # LeNet: 卷积神经网络
+# model_type = "AlexNet"  # AlexNet: 深度卷积神经网络
+model_type = "VGG"  # VGG: 使用重复元素的神经网络
 # model_type = "Conv1D+LSTM"  # Conv1D+LSTM：1 维卷积神经网络 + LSTM
 # model_type = "GlobalMaxPooling1D"  # GlobalMaxPooling1D：1 维全局池化层
 # model_type = "GlobalMaxPooling1D+MLP"  # GlobalMaxPooling1D+MLP：1 维全局池化层 + 多层感知机
 # model_type = "LSTM"  # LSTM：循环神经网络
 # model_type = "MLP"  # MLP：多层感知机
-model_type = 'Conv1D'
-RMSProp_lr = 5e-04
-epochs = 30
+learning_rate = 3e-04
+epochs = 20
 batch_size = 1024
 max_len = 256  # {64:803109，128:882952, 256:898459, 384:899686} 个用户；{64:1983350，128:2329077} 个素材
 embedding_size = 32
 embedding_window = 128
+show_parameter = False  # 显示模型参数
 
 # 与训练相关的参数
 field_list = [  # 输入数据处理：选择需要的列
@@ -73,8 +76,19 @@ load_file_path = '../../save_data/sparsity_hash/original/{}/'.format(creative_id
 load_file_name = load_file_path + 'train_data_all_output.csv'
 data_file_path = '../../save_data/sparsity_hash/no_interval/with_repeat/creative_id/{0}/{1}/'.format(creative_id_window, label_name)
 data_w2v_path = '../../save_data/sparsity_hash/word2vec/no_interval/with_repeat/creative_id/{0}/'.format(creative_id_window)
-model_file_path = '../../save_model/sparsity_hash/no_interval/with_repeat/word2vec/creative_id/{0}/{1}/'.format(creative_id_window, label_name)
+show_data = False  # 显示加载的数据
+show_result = True  # 显示训练结果
+model_file_path = '../../save_model/sparsity_hash/no_interval/with_repeat/word2vec/creative_id/{0}/{1}/{2}/'.format(creative_id_window, label_name, model_type)
 model_w2v_path = '../../save_model/sparsity_hash/word2vec/no_interval/with_repeat/creative_id/{0}/'.format(creative_id_window)
-
+model_file_prefix = 'embedding_{0}_{1}_'.format(embedding_size, max_len)
+save_model = False  # 保存训练模型
+x_train_file_name = 'x_train'  # 训练数据
+y_train_file_name = 'y_train'  # 训练数据
+train_data_type = '训练数据集'
+# x_train_file_name = 'x_train_balance'
+# y_train_file_name = 'y_train_balance'
+# train_data_type='平衡的训练数据集'
+x_test_file_name = 'x_test'  # 测试数据
+y_test_file_name = 'y_test'  # 测试数据
 balance_age_list = [11, 2, 1, 2, 2, 3, 5, 12, 20, 34]
 balance_gender_list = [0, 1]
