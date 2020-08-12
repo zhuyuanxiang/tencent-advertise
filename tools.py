342403f1b33c502f3b6abe56c94598c9c7ce3773
zYx.Tom <zhuyuanxiang@gmail.com>
2020-08-12 10:10:11 +0800

    工具函数

---
+++ tools.py
@@ -0,0 +1,40 @@
+# -*- encoding: utf-8 -*-    
+"""
+@Author     :   zYx.Tom
+@Contact    :   526614962@qq.com
+@site       :   https://zhuyuanxiang.github.io
+---------------------------
+@Software   :   PyCharm
+@Project    :   tencent-advertise
+@File       :   tools.py
+@Version    :   v0.1
+@Time       :   2020-08-11 10:50
+@License    :   (C)Copyright 2018-2020, zYx.Tom
+@Reference  :   
+@Desc       :   工具函数
+@理解：
+"""
+
+
+# common imports
+
+
+def beep_end():
+    # 运行结束的提醒
+    import winsound
+    winsound.Beep(600, 500)
+    pass
+
+
+def show_figures():
+    import matplotlib.pyplot as plt
+    # 运行结束前显示存在的图形
+    if len(plt.get_fignums()) != 0:
+        plt.show()
+    pass
+
+
+def show_title(message):
+    # 输出运行模块的标题
+    print('-' * 5, '>' + message + '<', '-' * 5)
+    pass
