a1619fa82328ea903b8f542e5be054f5137155d4
zYx.Tom <zhuyuanxiang@gmail.com>
2020-08-12 10:17:33 +0800

    generate_day_list_data() 生成每个用户每天访问数据列表，用于生成每天的均值数据(或者其他按天给出的统计特征)

--- generate_data.py
+++ generate_data.py
@@ -14,25 +14,24 @@
 @Desc       :   数据生成模块
 @理解：
 """
-import math
-
-import numpy as np
-from sklearn.model_selection import train_test_split
-
 import config
-from config import creative_id_max, time_id_max, user_id_num
-from config import seed
+import math
+import numpy as np
+
+from config import creative_id_max, time_id_max, user_id_max, fix_period_days, fix_period_length
 from config import creative_id_window, creative_id_begin, creative_id_end
+from config import seed
 from show_data import show_example_data
+from sklearn.model_selection import train_test_split
 
 
 # ----------------------------------------------------------------------
-def generate_word2vec_data_with_interval(x_csv, creative_id_begin, creative_id_end):
+def generate_word2vec_data_with_interval(x_csv):
     print('-' * 5 + "   清洗数据集:{0}~{1} 个素材   ".format(creative_id_begin, creative_id_end) + '-' * 5)
     data_length = x_csv.shape[0]
     data_step = data_length // 100  # 标识数据清洗进度的步长
     print("数据生成中（共 {0} 条数据)：".format(data_length), end='')
-    x_creative_id = [[chr(2)] for _ in range(user_id_num)]
+    x_creative_id = [[chr(2)] for _ in range(user_id_max)]
     prev_user_id = -1  # -1 不在数据序列中
     prev_time_id = -1  # 0 表示第 1 天
     time_id_interval = 0
@@ -77,12 +76,12 @@ def generate_word2vec_data_with_interval(x_csv, creative_id_begin, creative_id_e
 
 
 # ----------------------------------------------------------------------
-def generate_word2vec_data_no_interval(x_csv, creative_id_begin, creative_id_end):
+def generate_word2vec_data_no_interval(x_csv):
     print('-' * 5 + "   清洗数据集:{0}~{1} 个素材   ".format(creative_id_begin, creative_id_end) + '-' * 5)
     data_length = x_csv.shape[0]
     data_step = data_length // 100  # 标识数据清洗进度的步长
     print("数据生成中（共 {0} 条数据)：".format(data_length), end='')
-    x_creative_id = [[chr(2)] for _ in range(user_id_num)]  # 每个序列都以 2
+    x_creative_id = [[chr(2)] for _ in range(user_id_max)]  # 每个序列都以 2
     prev_user_id = -1  # -1 不在数据序列中
     creative_id_list = None
     for i, row_data in enumerate(x_csv):
@@ -116,7 +115,34 @@ def generate_word2vec_data_no_interval(x_csv, creative_id_begin, creative_id_end
 
 
 # ----------------------------------------------------------------------
-def generate_fix_data(X_csv, y_csv, train_field_num, label_field_num, period_length, period_days, creative_id_begin, creative_id_end):
+def generate_day_list_data(X_csv, y_csv):
+    """
+    生成每个用户每天访问数据列表，用于生成每天的均值数据(或者其他按天给出的统计特征)
+    :param X_csv:
+    :param y_csv:
+    """
+    from tools import show_title
+    show_title('清洗数据集')
+    data_length = y_csv.shape[0]
+    data_step = data_length // 100
+    print("数据生成中（共 {0} 条数据)：".format(data_length), end='')
+    X_doc = np.empty([user_id_max, time_id_max], dtype=object)
+    y_doc = np.empty([user_id_max, ], dtype=int)
+    prev_user_id = -1
+    creative_id_list = None
+    for i, row_data in enumerate(X_csv):
+        if (i % data_step) == 0:  # 数据清洗的进度
+            print("第 {0} 条数据-->".format(i), end=';')
+            pass
+        user_id = row_data[0]
+        creative_id = row_data[1]
+        time_id = row_data[2]
+
+        creative_id_list = X_doc[user_id, time_id]
+
+
+# ----------------------------------------------------------------------
+def generate_fix_data(X_csv, y_csv):
     print('-' * 5 + "   清洗数据集   " + '-' * 5)
     data_length = y_csv.shape[0]
     data_step = data_length // 100  # 标识数据清洗进度的步长
@@ -124,11 +150,12 @@ def generate_fix_data(X_csv, y_csv, train_field_num, label_field_num, period_len
     # 初始化 X_doc 为空的列表 : X_doc[:,0]: creative_id, X_doc[:,1]: click_times
     # 初始化 y_doc 为空的列表 : y_doc[:,0]: age, X_doc[:,1]: gender
     # X_doc 的列表维度 = 去除 user_id, time_id
-    X_doc = np.zeros([user_id_num, train_field_num, math.ceil(time_id_max / period_days) * period_length], dtype=object)
-    y_doc = np.zeros([user_id_num, label_field_num], dtype=int)
-    prev_user_id = -1
-    prev_time_id = 0
-    period_index = 0
+    X_doc = np.zeros([user_id_max, math.ceil(time_id_max / fix_period_days) * fix_period_length], dtype=object)
+    y_doc = np.zeros([user_id_max, ], dtype=int)
+    prev_user_id = 0  # 前一条数据的用户编号
+    prev_time_id = 0  # 前一条数据的日期编号
+    prev_period_index = 0  # 周期索引，即处理到第几个周期
+    period_data_index = 0  # 周期内数据保存的位置索引
     for i, row_data in enumerate(X_csv):
         if (i % data_step) == 0:  # 数据清洗的进度
             print("第 {0} 条数据-->".format(i), end=';')
@@ -136,54 +163,43 @@ def generate_fix_data(X_csv, y_csv, train_field_num, label_field_num, period_len
         user_id = row_data[0]
         time_id = row_data[2]
 
-        for j in range(label_field_num):
-            y_doc[user_id, j] = y_csv[i, j]
-
         # 整理过的数据已经按照 user_id 的顺序编号，当 user_id 变化时，就代表前一个用户的数据已经清洗完成
         if user_id > prev_user_id:
             # 重置临时变量
             prev_user_id = user_id
             prev_time_id = 0
-            period_index = 0
+            period_data_index = 0
             pass
 
-        if (time_id - prev_time_id) >= period_days:
-            prev_time_id = time_id // period_days * period_days
-            period_index = 0
+        if (time_id - prev_period_index) >= fix_period_days:
+            prev_period_index = time_id // fix_period_days * fix_period_days
 
-        if period_index == period_length:  # 每个周期访问素材填满后不再填充
-            continue
+        if period_data_index < fix_period_length:  # 每个周期访问素材填满后不再填充
+            # creative_id 的值已经在读取时修正过，增加了偏移量 3，保留了 {0,1,2}
+            #   数据在 SQL 阶段已经处理了，不在字典中的数据已经置为1
+            #   2 这个值在这个序列中没有使用，但是为了保持数据处理的统一性，没有修改
+            # row_data[0]: user_id
+            creative_id = row_data[1]
+            # row_data[2]: time_id
+            click_times = row_data[3]  # 这个不是词典，本身就是值，不需要再调整
 
-        # row_data[0]: user_id
-        creative_id = row_data[1]  # 这个值已经在读取时修正过，增加了偏移量 2，保留了 {0,1}
-        # row_data[2]: time_id
-        click_times = row_data[3]  # 这个不是词典，本身就是值，不需要再调整
+            for _ in range(click_times):
+                X_doc[user_id, prev_period_index + period_data_index] = creative_id
+                period_data_index = period_data_index + 1
 
-        # 素材是关键
-        if creative_id_end > creative_id > creative_id_begin:
-            creative_id = creative_id - creative_id_begin
-        elif creative_id < creative_id_end - creative_id_max:
-            creative_id = creative_id_max - creative_id_begin + creative_id
-        else:
-            creative_id = 1  # 超过词典大小的素材标注为 1，即「未知」
-
-        X_doc[user_id, 0, (time_id // period_days) * period_length + period_index] = creative_id
-        X_doc[user_id, 1, (time_id // period_days) * period_length + period_index] = click_times
-        period_index = period_index + 1
-        pass
     print("\n数据清洗完成！")
     show_example_data(X_doc, y_doc, data_type='清洗数据')
     return X_doc, y_doc
 
 
 # ----------------------------------------------------------------------
-def generate_no_time_data(X_csv, y_csv, train_field_num, label_field_num, repeat_creative_id, creative_id_begin, creative_id_end):
+def generate_no_time_data(X_csv, y_csv, train_field_num, label_field_num, repeat_creative_id):
     print('-' * 5 + "   清洗数据集   " + '-' * 5)
     data_length = y_csv.shape[0]
     data_step = data_length // 100  # 标识数据清洗进度的步长
     print("数据生成中（共 {0} 条数据)：".format(data_length), end='')
-    X_doc = np.zeros([user_id_num, train_field_num], dtype=object)
-    y_doc = np.zeros([user_id_num, label_field_num], dtype=int)
+    X_doc = np.zeros([user_id_max, train_field_num], dtype=object)
+    y_doc = np.zeros([user_id_max, label_field_num], dtype=int)
     # -1 不在数据序列中
     prev_user_id = -1
     for i, row_data in enumerate(X_csv):
