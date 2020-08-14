571bb52ecf148096d56c92f87f7ae90bee4c8331
zYx.Tom <zhuyuanxiang@gmail.com>
2020-08-14 16:05:52 +0800

    移动与 MLP 相关的建模函数到这个模块中

---
+++ builld_model_MLP.py
@@ -0,0 +1,42 @@
+# -*- encoding: utf-8 -*-    
+"""
+@Author     :   zYx.Tom
+@Contact    :   526614962@qq.com
+@site       :   https://zhuyuanxiang.github.io
+---------------------------
+@Software   :   PyCharm
+@Project    :   tencent-advertise
+@File       :   builld_model_MLP.py
+@Version    :   v0.1
+@Time       :   2020-08-14 11:26
+@License    :   (C)Copyright 2018-2020, zYx.Tom
+@Reference  :   
+@Desc       :   多层前向神经网络
+@理解：
+"""
+# common imports
+from keras.layers import Flatten, Dropout, Dense
+from keras.regularizers import l2
+
+import config
+import tools
+from tensorflow import keras
+
+# ----------------------------------------------------------------------
+# ----------------------------------------------------------------------
+# ----------------------------------------------------------------------
+from build_model import build_single_input, build_single_output
+from config import embedding_size
+
+if __name__ == '__main__':
+    # 运行结束的提醒
+    tools.beep_end()
+
+
+def build_mlp():
+    model = build_single_input()
+    model.add(Flatten())
+    model.add(Dropout(0.5))
+    model.add(Dense(embedding_size * 4, activation='relu', kernel_regularizer=l2(0.001)))
+    model.add(Dropout(0.5))
+    return build_single_output(model)
