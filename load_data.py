4df3ae05f03391be5ad17cc97e51b196869ea778
zYx.Tom <zhuyuanxiang@gmail.com>
2020-08-14 16:16:25 +0800

    增加对 product_category 的载入

--- load_data.py
+++ load_data.py
@@ -17,7 +17,7 @@
 import config
 import numpy as np
 import pandas as pd
-from show_data import show_example_data, show_word2vec_data, show_original_x_data, show_original_data
+from show_data import show_example_data, show_word2vec_data, show_original_x_data, show_original_data, show_data_result
 
 
 # ----------------------------------------------------------------------
@@ -30,18 +30,18 @@ def load_original_data():
     df = pd.read_csv(config.load_file_name, dtype=int)
     # --------------------------------------------------
     # 没有在数据库中处理索引，是因为尽量不在数据库中修正原始数据，除非是不得不变更的数据，这样子业务逻辑清楚
-    # user_id:          X_csv[:,0]
+    # user_id:          X_csv[:,0]      需要减 1
     # creative_id:      X_csv[:,1]
-    # time_id:          X_csv[:,2]
-    # click_times:      X_csv[:,3]
+    # time_id:          X_csv[:,2]      需要减 1
+    # click_times:      X_csv[:,3]      数据已经是值，不是索引，不需要减 1
+    # product_category: X_csv[:,4]      0 保留不使用，因此不需要减 1
     # 索引在数据库中是从 1 开始的，在 Python 中是从 0 开始的，因此字段的偏移量为 -1
     x_csv = df[config.field_list].values
     # 'creative_id_inc' 字段的偏移量为 3，是因为需要保留 {0, 1, 2}：0 表示 “padding”（填充），1 表示 “unknown”（未知词），2 表示 “start”（序列开始）
     # TODO：建议 SQL 导出数据前就已经将数据序号改成从0开始，将 creative_id 的序号保留 0，1，2
-    x_csv[:, 0] = x_csv[:, 0] - 1  # user_id 需要减 1
+    x_csv[:, 0] = x_csv[:, 0] - 1  # user_id
     x_csv[:, 1] = x_csv[:, 1] + (3 - 1)
-    x_csv[:, 2] = x_csv[:, 2] - 1  # time_id 需要减 1
-    x_csv[:, 3] = x_csv[:, 3] + (1 - 1)  # click_times 的数据已经是值，不是索引，不需要减 1
+    x_csv[:, 2] = x_csv[:, 2] - 1  # time_id
     # --------------------------------------------------
     # 目标数据处理：目标字段的偏移量是 -1，是因为索引在数据库中是从 1 开始的，在 Python 中是从 0 开始的
     # 既加载了 'age'，也加载了 'gender'
@@ -111,11 +111,8 @@ def load_data_set(file_path):
 
 
 def load_data(file_name, data_type='原始数据集'):
-    print("加载{0}：{1}-->".format(data_type, file_name), end='')
+    print("加载{0}：{1}.npy --> ".format(data_type, file_name), end='')
     data = np.load(file_name + '.npy', allow_pickle=True)
-    print(data_type + ':{}-->加载成功！'.format(len(data)))
-    if isinstance(data[0], list) and isinstance(data[0][0], str):
-        show_word2vec_data(data, data_type)
-    else:
-        show_original_data(data, data_type)
+    print(data_type + ':{}条数据 -->加载成功！'.format(len(data)))
+    show_data_result(data, data_type)
     return data
