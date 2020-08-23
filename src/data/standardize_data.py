eef05ac760765506f7bf82479fdb186efdddc3e7
zYx.Tom <zhuyuanxiang@gmail.com>
2020-08-23 09:54:56 +0800

    增加 数据标准化模块，用于将统计特征规范到0~1之间

---
+++ src/data/standardize_data.py
@@ -0,0 +1,26 @@
+# -*- encoding: utf-8 -*-    
+"""
+@Author     :   zYx.Tom
+@Contact    :   526614962@qq.com
+@site       :   https://zhuyuanxiang.github.io
+---------------------------
+@Software   :   PyCharm
+@Project    :   tencent-advertise
+@File       :   standardize_data.py
+@Version    :   v0.1
+@Time       :   2020-08-21 12:12
+@License    :   (C)Copyright 2018-2020, zYx.Tom
+@Reference  :   
+@Desc       :   数据标准化模块
+@理解：
+"""
+import config
+import tools
+from tensorflow import keras
+
+# ----------------------------------------------------------------------
+# ----------------------------------------------------------------------
+# ----------------------------------------------------------------------
+if __name__ == '__main__':
+    # 运行结束的提醒
+    tools.beep_end()
