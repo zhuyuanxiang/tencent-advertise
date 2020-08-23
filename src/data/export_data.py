0873f25e57cf4d66193c8711c5a8a3919e8d0012
zYx.Tom <zhuyuanxiang@gmail.com>
2020-08-23 09:55:44 +0800

    重构硬编码变量

--- src/data/export_data.py
+++ src/data/export_data.py
@@ -17,6 +17,7 @@
 from sklearn.model_selection import train_test_split
 
 import config
+from config import label_name
 from src.data.save_data import save_model_data
 from src.无用代码.generate_data import generate_balance_data
 from tools import show_title
@@ -24,27 +25,27 @@ from tools import show_title
 
 # ----------------------------------------------------------------------
 def export_val_balance(x_train_val, y_train_val):
-    show_title("平衡拆分验证的训练数据集({})的类别".format(config.label_name))
+    show_title(f"对类别 ：{label_name}实施平衡{config.train_val_data_type}")
     x_train_val_balance, y_train_val_balance = generate_balance_data(x_train_val, y_train_val)
     save_model_data(x_train_val_balance, config.data_file_path + 'x_train_val_balance', '平衡拆分验证的训练数据集')
     save_model_data(y_train_val_balance, config.data_file_path + 'y_train_val_balance', '平衡拆分验证的训练数据集')
 
 
 def export_train_balance(x_train, y_train):
-    show_title("平衡训练数据集({})的类别".format(config.label_name))
+    show_title(f"对类别 ：{label_name}实施平衡{config.train_data_type}")
     x_train_balance, y_train_balance = generate_balance_data(x_train, y_train)
-    save_model_data(x_train_balance, config.data_file_path + 'x_train_balance', '平衡的训练数据集')
-    save_model_data(y_train_balance, config.data_file_path + 'y_train_balance', '平衡的训练数据集')
+    save_model_data(x_train_balance, config.data_file_path + config.x_train_balance_file_name, '平衡的训练数据集')
+    save_model_data(y_train_balance, config.data_file_path + config.y_train_balance_file_name, '平衡的训练数据集')
 
 
 def export_val_data(x_train, y_train):
     show_title("拆分训练数据集和验证数据集")
     x_train_val, x_val, y_train_val, y_val = train_test_split(
             x_train, y_train, random_state=config.seed, stratify=y_train)
-    save_model_data(x_train_val, config.data_file_path + 'x_train_val', '无验证训练数据集')
-    save_model_data(y_train_val, config.data_file_path + 'y_train_val', '无验证训练数据集')
-    save_model_data(x_val, config.data_file_path + 'x_val', '验证数据集')
-    save_model_data(y_val, config.data_file_path + 'y_val', '验证数据集')
+    save_model_data(x_train_val, config.data_file_path + config.x_train_val_file_name, '无验证训练数据集')
+    save_model_data(y_train_val, config.data_file_path + config.y_train_val_file_name, '无验证训练数据集')
+    save_model_data(x_val, config.data_file_path + config.x_val_file_name, '验证数据集')
+    save_model_data(y_val, config.data_file_path + config.y_val_file_name, '验证数据集')
     return x_train_val, y_train_val
 
 
@@ -52,18 +53,22 @@ def export_train_test_data(x_data, y_data):
     show_title("拆分训练数据集和测试数据集")
     x_train, x_test, y_train, y_test = train_test_split(
             x_data, y_data, random_state=config.seed, stratify=y_data)
-    save_model_data(x_train, config.data_file_path + 'x_train', '训练数据集')
-    save_model_data(y_train, config.data_file_path + 'y_train', '训练数据集')
-    save_model_data(x_test, config.data_file_path + 'x_test', '测试数据集')
-    save_model_data(y_test, config.data_file_path + 'y_test', '测试数据集')
+    from sklearn.preprocessing import MinMaxScaler
+    scaler = MinMaxScaler()
+    scaler.fit(x_train)
+    x_train = scaler.transform(x_train)
+    save_model_data(x_train, config.data_file_path + config.x_train_file_name, '训练数据集')
+    save_model_data(y_train, config.data_file_path + config.y_train_file_name, '训练数据集')
+    save_model_data(x_test, config.data_file_path + config.x_test_file_name, '测试数据集')
+    save_model_data(y_test, config.data_file_path + config.y_test_file_name, '测试数据集')
     return x_train, y_train
 
 
 def export_generate_data(lst_data):
     from src.data.generate_data import generate_day_sequence_data
-    show_title("加工数据为 91 天的序列数据，每天为3个特征(最大值、最小值、平均值)96维数据")
+    show_title("加工数据为 91 天的序列数据，每天为6个特征(最大值、最小值、平均值)96维数据")
     x_data = generate_day_sequence_data(lst_data)
-    save_model_data(x_data, config.data_file_path + 'x_data', '中间数据集')
+    save_model_data(x_data, config.data_file_path + config.x_data_file_name, '中间数据集')
 
     # show_title("加工数据为定长的数据列表")
     # from src.data.generate_data import generate_fix_data
@@ -77,8 +82,10 @@ def export_generate_data(lst_data):
 
 def export_base_data():
     lst_data, y_data = export_day_list_data()
-    x_data = export_generate_data(lst_data)
     export_w2v_data(lst_data)
+    # from src.data.load_data import load_day_list_data
+    # lst_data, y_data = load_day_list_data()
+    x_data = export_generate_data(lst_data)
     return x_data, y_data
 
 
@@ -90,8 +97,8 @@ def export_day_list_data():
     from src.data.generate_data import generate_day_list_data
     show_title("导出每个用户每天访问数据的不截断列表")
     lst_data, y_data = generate_day_list_data(x_csv, y_csv)
-    save_model_data(lst_data, config.data_file_path + 'lst_data', '中间数据集')
-    save_model_data(y_data, config.data_file_path + 'y_data', '中间数据集')
+    save_model_data(lst_data, config.data_file_path + config.lst_data_file_name, '中间数据集')
+    save_model_data(y_data, config.data_file_path + config.y_data_file_name, '中间数据集')
     return lst_data, y_data
 
 
